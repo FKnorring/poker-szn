c256ff5294a793bc2202c4a57308a822473b8b12
FKnorring <fille.vk@gmail.com>
2024-02-08 18:01:52 +0100

    Swedish localestrings

--- app/edit/components/list-game.tsx
+++ app/edit/components/list-game.tsx
@@ -3,7 +3,7 @@ import { Game } from "@prisma/client";
 export default function ListGame({ game }: { game: Game }) {
   return (
     <div className="flex">
-      <h2>{game.date.toLocaleDateString()}</h2>
+      <h2>{game.date.toLocaleDateString("sv-SE")}</h2>
     </div>
   );
 }
