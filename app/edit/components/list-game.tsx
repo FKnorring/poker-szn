9e4dbdc54224a2a81333799c7522e55d733ec2e4
FKnorring <fille.vk@gmail.com>
2024-02-08 18:08:08 +0100

    Added icons for games in list

--- app/edit/components/list-game.tsx
+++ app/edit/components/list-game.tsx
@@ -1,9 +1,17 @@
 import { Game } from "@prisma/client";
+import { ExtendedGame } from "../games";
+import { User } from "lucide-react";
 
-export default function ListGame({ game }: { game: Game }) {
+export default function ListGame({ game }: { game: ExtendedGame }) {
   return (
-    <div className="flex">
+    <div className="flex flex-grow justify-between pe-2">
       <h2>{game.date.toLocaleDateString("sv-SE")}</h2>
+      <div className="flex items-center gap-4">
+        +/-
+        <div className="flex items-center">
+          {game.players.length} <User size={16} />
+        </div>
+      </div>
     </div>
   );
 }
