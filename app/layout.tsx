8d4de69a86904cd60c1c96769f0f4ed5e0a6d596
FKnorring <fille.vk@gmail.com>
2024-02-09 22:56:18 +0100

    Changed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Poker SZN 2024",
@@ -17,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={GeistSans.className}>{children}</body>
       <Toaster />
     </html>
   );
