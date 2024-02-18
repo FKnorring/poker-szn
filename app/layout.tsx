7740fb85d04388557cb91a02c866260017d87e70
FKnorring <fille.vk@gmail.com>
2024-02-18 14:10:28 +0100

    Added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from "geist/font/sans";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Poker SZN 24",
@@ -27,6 +28,7 @@ export default function RootLayout({
             {children}
           </main>
         </ThemeProvider>
+        <SpeedInsights />
       </body>
       <Toaster />
     </html>
