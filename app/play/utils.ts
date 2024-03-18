b195e10a7ea54518a07eb9d9746c8855c2ce8a2c
FKnorring <fille.vk@gmail.com>
2024-03-18 18:19:27 +0100

    Test of date fix?

--- app/play/utils.ts
+++ app/play/utils.ts
@@ -1,6 +1,6 @@
 export function isTimeToPlay(date: Date) {
   const nextDay = new Date(date);
-  nextDay.setDate(nextDay.getDate() + 1);
+  nextDay.setDate(nextDay.getDate() + 2);
   nextDay.setHours(6, 0, 0, 0);
   return Date.now() < nextDay.getTime();
 }
